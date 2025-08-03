3840017c10490b25faa259e689402aee08d2da1c
Shaaban Hamdy <shabanhamdy94@gmail.com>
2025-08-04 01:45:34 +0300

    first commit

--- db/Connectiondb.js
+++ db/Connectiondb.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const Connectiondb = async () => {
   return await mongoose
-    .connect(process.env.MONGODB_URI )
+    .connect(process.env.MONGODB )
     .then((res) => console.log("ConnectionDB is Running........"))
     .catch((err) => console.log({ message: "fail in ConnectionDB", err }));
 };
 
 export default Connectiondb;
+
 
